2ed42a4cc649c77e3a3c6bee6788a32f49ca5a38
fabianmontes <fabianmontes11fab@gmail.com>
2024-07-13 00:44:58 -0600

    conexion a base de datos

---
+++ src/main/java/com/aluracursos/literalura/modelos/Autor.java
@@ -0,0 +1,35 @@
+package com.aluracursos.literalura.modelos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "autor")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private Integer fechaNacimiento;
+    private Integer fechaMuerte;
+    private String nombre;
+    
+    public Autor(DatosAutor datosAutor) {
+        fechaNacimiento = datosAutor.fechaNacimiento();
+        fechaMuerte = datosAutor.fechaMuerte();
+        nombre = datosAutor.nombre();
+    }
+
+    public Integer getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(Integer fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+    
+    public Integer getFechaMuerte() { return fechaMuerte; }
+    public void setFechaMuerte(Integer fechaMuerte) { this.fechaMuerte = fechaMuerte; }
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+        
+    @Override
+    public String toString() {
+        return getNombre();
+    }
+}
--- src/main/java/com/aluracursos/literalura/modelos/Libro.java
+++ src/main/java/com/aluracursos/literalura/modelos/Libro.java
@@ -1,16 +1,25 @@
 package com.aluracursos.literalura.modelos;
 
 import java.util.List;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "libro")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(unique = true)
     private String titulo;
-    private List<DatosAutor> autor;
+
     private List<String> idiomas;
     private Integer numeroDescargas;
+    @Transient
+    private List<Autor> autores;
 
     public Libro(DatosLibro datosLibro) {
         this.titulo = datosLibro.titulo();
-        this.autor = datosLibro.autor();
         this.idiomas = datosLibro.idiomas();
         this.numeroDescargas = datosLibro.numeroDescargas();
     }
@@ -19,7 +28,7 @@ public class Libro {
     public String toString() {
         return  "----- LIBRO -----\n"+
                 " Título: "+titulo+"\n"+
-                " Autor: "+"\n"+
+                " Autor: "+"\n"+autores+
                 " Idioma: "+idiomas+"\n"+
                 " Numero de descargas: "+numeroDescargas+"\n"+
                 "-----------------\n";
